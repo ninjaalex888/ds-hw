2a5f1ed998693b604868db4a433c88230666491a
ninjaalex888 <alex.sheehan@colorado.edu>
2016-09-08 01:25:16 -0600

    updates

--- wrangling/word_counts.py
+++ wrangling/word_counts.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from zipfile import ZipFile
 import re
+import zipfile
 
 kWORDS = re.compile("[a-z]{4,}")
 
@@ -10,7 +11,22 @@ def text_from_zipfile(zip_file):
     zip file.
     """
     # Modify this function
-    return ["nope"]
+    files = {}
+    #zipVar = zipfile.ZipFile(zip_file)
+    with zipfile.ZipFile(zip_file) as zippedFile:
+        for name in zippedFile.namelist():
+            #print(name)
+            if name != "state_union/README":
+                if name != "state_union/":
+                    #files[name] = zippedFile.read(name)
+                    bytesObj = zippedFile.read(name)
+                    #bytesObj.encode('utf-8').strip()
+                    #string = str(bytesObj,'utf-8')
+                    #files[name] = str(bytesObj)
+                    yield str(bytesObj)
+    
+    #print(type(files['state_union/1945-Truman.txt']))
+    #return ["nope"]
 
 def words(text):
     """
@@ -18,8 +34,35 @@ def words(text):
     characters in the range a-z or A-Z.  The resulting words should be
     lower case.
     """
+    # s = """Return all words in a string, where a word is four or more contiguous
+    # characters in the range a-z or A-Z.  The resulting words should be
+    # lower case."""
+    #print(text)
+    # removedText = re.compile("(\w[\w']*\w|\w)") 
+    # text = removedText.findall(text)
+    text = "Yes, we can certainly find real words, Frank!"
+    passedWords =[]
+    tempList = text.split()
+    for word in tempList:
+        passedWord = ""
+        for char in word:
+            if char in '!,.?":;0123456789':
+                char = ""
+            passedWord += char
+        if len(passedWord) > 3:
+            print(passedWord)
+            passedWords.append(passedWord)
+
+    text = passedWords
+    text = [x.lower() for x in text]
+    #shortword = re.compile(r'\W*\b\w{1,3}\b')    
+    #shortword.sub('', text)
+    #text = [x for x in text if len(x) > 4] #remove words < 4 letters
+
+    print(text)
+    
     # Modify this function
-    return text.lower().split()
+    return text
 
 def accumulate_counts(words, total=Counter()):
     """
